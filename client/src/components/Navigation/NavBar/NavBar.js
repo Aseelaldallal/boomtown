067b7084b94047acb350f048ccf03029449cd8b7
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-01-12 16:32:17 -0500

    change Navbar to container

--- client/src/components/Navigation/NavBar/NavBar.js
+++ client/src/components/Navigation/NavBar/NavBar.js
@@ -1,67 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import Logo from '../../../images/boomtown-logo.svg';
-import DropDownMenu from 'material-ui/DropDownMenu';
-// import DropDownMenu from '../../UI/DropDownMenu/DropDownMenu';
-import MenuItem from 'material-ui/MenuItem';
 import Button from '../../UI/Button/Button';
 import Auxillary from '../../../hoc/Auxillary/Auxillary';
 
 import './NavBar.css';
 
-const NavBar = () => {
-  const styles = {
-    logoHeight: {
-      height: 40
-    },
-    appBar: {
-      backgroundColor: 'white',
-      height: 75,
-      padding: '10px 40px'
-    },
-    dropDownMenu: {
-      textAlign: 'left',
-      width: 350,
-      color: 'black'
-    },
-    buttonMargin: {
-      marginRight: '1rem'
-    },
-    button: {
-      backgroundColor: 'black',
-      color: 'white'
-    }
-  };
+class NavBar extends Component {
+  render() {
+    const styles = {
+      logoHeight: {
+        height: 40
+      },
+      appBar: {
+        backgroundColor: 'white',
+        height: 75,
+        padding: '10px 40px'
+      },
+      dropDownMenu: {
+        textAlign: 'left',
+        width: 350,
+        color: 'black'
+      },
+      buttonMargin: {
+        marginRight: '1rem'
+      },
+      button: {
+        backgroundColor: 'black',
+        color: 'white'
+      }
+    };
 
-  const dropDownMenu = (
-    <DropDownMenu
-      value={'HELLOOOOOOOOOO'}
-      style={styles.dropDownMenu}
-      autoWidth={false}
-    >
-      <MenuItem value={1} primaryText="Morning" />
-      <MenuItem value={2} primaryText="Hard Coded" />
-      <MenuItem value={3} primaryText="Hard Coded" />
-      <MenuItem value={4} primaryText="Hard Coded" />
-      <MenuItem value={5} primaryText="Hard Coded" />
-    </DropDownMenu>
-  );
+    const buttons = (
+      <Auxillary>
+        <Button type="buttonBlue">My Profile</Button>
+        <Button type="buttonBlack">Logout </Button>
+      </Auxillary>
+    );
 
-  const buttons = (
-    <Auxillary>
-      <Button type="buttonBlue">My Profile</Button>
-      <Button type="buttonBlack">Logout </Button>
-    </Auxillary>
-  );
-
-  return (
-    <AppBar
-      style={styles.appBar}
-      title={dropDownMenu}
-      iconElementLeft={<img style={styles.logoHeight} src={Logo} alt="logo" />}
-      iconElementRight={buttons}
-    />
-  );
-};
+    return (
+      <AppBar
+        style={styles.appBar}
+        title="Select goes here"
+        iconElementLeft={
+          <img style={styles.logoHeight} src={Logo} alt="logo" />
+        }
+        iconElementRight={buttons}
+      />
+    );
+  }
+}
 
 export default NavBar;
