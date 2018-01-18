557e9a7489dfb1ead09482180ec3aac1e998add0
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-01-18 12:47:07 -0500

    set up redux, react-redux, store

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // React Router
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// Redux
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import itemsReducer from './store/reducers/items';
 // Material UI, Styling
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import muiTheme from './config/theme';
@@ -16,19 +21,35 @@ import ItemAdder from './containers/Items/ItemAdder/ItemAdder';
 // Register Service Worker
 import registerServiceWorker from './registerServiceWorker';
 
+const rootReducer = combineReducers({
+  items: itemsReducer
+});
+
+const composeEnhancers =
+  (process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null) || compose; // compose is a fallback
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
 const Boomtown = () => (
-  <BrowserRouter>
-    <Switch>
-      <MuiThemeProvider muiTheme={muiTheme}>
-        <Layout>
-          <Route path="/login" component={Login} />
-          <Route exact path="/items" component={ItemsContainer} />
-          <Route exact path="/profile/:userid" component={UserProfile} />
-          <Route exact path="/share" component={ItemAdder} />
-        </Layout>
-      </MuiThemeProvider>
-    </Switch>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <MuiThemeProvider muiTheme={muiTheme}>
+          <Layout>
+            <Route path="/login" component={Login} />
+            <Route exact path="/items" component={ItemsContainer} />
+            <Route exact path="/profile/:userid" component={UserProfile} />
+            <Route exact path="/share" component={ItemAdder} />
+          </Layout>
+        </MuiThemeProvider>
+      </Switch>
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(<Boomtown />, document.getElementById('root'));
