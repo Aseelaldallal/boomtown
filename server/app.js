810e7af4deb7cd26e86c4fdacb89b480e4175a96
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-01-26 11:55:13 -0500

    modify routes

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 // BASE SETUP
 // =============================================================================
 
-// call the packages we need
+// Call the packages we need
 let express = require('express'); // call express
-let app = express(); // define our app using express
 let bodyParser = require('body-parser');
+
+// define our app using express
+let app = express(); 
+
+// Routes
 let itemRoutes = require('./routes/item');
 
 // configure app to use bodyParser()
@@ -12,17 +16,13 @@ let itemRoutes = require('./routes/item');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// var port = 3001; // set our port
-
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /
 app.use('/items', itemRoutes);
 
 // START THE SERVER
 // =============================================================================
-// app.listen(port);
-// console.log('Magic happens on port ' + port);
 
 app.listen(3001, function() {
-  console.log('Event App Server Has Started!');
+  console.log('Listening!');
 });
