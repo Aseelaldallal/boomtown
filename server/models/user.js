a34a5ef7af163bb80506484e6f77c0d8c2d8a8ab
unknown <aseelaldallal@gmail.com>
2018-01-28 11:24:26 -0500

    temp change user model to only local email and password

--- server/models/user.js
+++ server/models/user.js
@@ -2,19 +2,18 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   local: {
-    username: String,
     email: String,
     password: String
-  },
-  email: {
-    type: String
-  },
-  fullname: {
-    type: String
-  },
-  bio: {
-    type: String
   }
+  // email: {
+  //   type: String
+  // },
+  // fullname: {
+  //   type: String
+  // },
+  // bio: {
+  //   type: String
+  // }
 });
 
 module.exports = mongoose.model('user', UserSchema);
