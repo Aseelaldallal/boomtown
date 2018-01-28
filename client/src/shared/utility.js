a46ac67774b12371e7aba22a2a006e3253070654
unknown <aseelaldallal@gmail.com>
2018-01-28 17:50:39 -0500

    Create checkvalidity method

--- client/src/shared/utility.js
+++ client/src/shared/utility.js
@@ -4,3 +4,25 @@ export const updateObject = (oldObject, updatedProperties) => {
     ...updatedProperties
   };
 };
+
+
+export const checkValidity = (value, rules) => {
+  let isValid = true;
+  if (!rules) {
+    return true;
+  }
+  if (rules.required) {
+    isValid = (value.trim() !== '') && isValid;
+  }
+  if (rules.minLength) {
+    isValid = (value.replace(/ /g, '').length >= rules.minLength) && isValid;
+  }
+  if (rules.maxLength) {
+    isValid = (value.replace(/ /g, '').length <= rules.maxLength) && isValid;
+  }
+  if (rules.isEmail) {
+    const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    isValid = pattern.test(value) && isValid
+  }
+  return isValid;
+}
