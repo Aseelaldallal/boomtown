f8a48163616681c091e29f60036bcc1cb10e10c9
unknown <aseelaldallal@gmail.com>
2018-01-27 19:04:11 -0500

    Add Item Route

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 // =============================================================================
-// BASIC SETUP
+// SETUP
 // =============================================================================
+
+// Port
+const PORT = process.env.PORT || 3001;
 
 // Get Packages
 const mongoose = require('./db/mongoose'),
@@ -10,8 +13,19 @@ const mongoose = require('./db/mongoose'),
 // Get Express Instance
 const app = express();
 
-// Port
-const PORT = process.env.PORT || 3001;
+// Get Routes
+let itemRoutes = require('./routes/item');
+
+// =============================================================================
+// MIDDLEWARE
+// =============================================================================
+
+// Parse client request body - turn it into JSON
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Register Routes
+app.use('/items', itemRoutes); // All routes inside itemRoutes are now prefixed with /items
 
 // =============================================================================
 // LISTEN
@@ -21,21 +35,3 @@ app.listen(PORT, function () {
   console.log(`Server started on port ${PORT}`);
 });
 
-// // Routes
-// let itemRoutes = require('./routes/item');
-
-// // configure app to use bodyParser()
-// // this will let us get the data from a POST
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-
-// // REGISTER OUR ROUTES -------------------------------
-// // all of our routes will be prefixed with /
-// app.use('/items', itemRoutes);
-
-// // START THE SERVER
-// // =============================================================================
-
-// app.listen(3001, function() {
-//   console.log('Listening!');
-// });
--- server/routes/item.js
+++ server/routes/item.js
@@ -1,68 +1,24 @@
-/***************************************************/
-/********************* SETUP ***********************/
-/***************************************************/
+// ===============================================
+// SETUP
+// ===============================================
 
 var express = require('express'),
-  Item = require('../models/item'),
+  Items = require('../models/item'),
   router = express.Router();
 
-/* ----------------- INDEX ROUTE ----------------- */
+// ===============================================
+// Index: Display All Items
+// ===============================================
 
-// Display All Items
-router.get('/', function(req, res) {
-  res.send('Index');
+
+router.get('/', function (req, res) {
+  Items.find({}).then(foundItems => {
+    res.send(foundItems);
+  }).catch(err => {
+    console.error(err);
+  })
 });
 
-/* ----------------- NEW ROUTE ----------------- */
-// NOT WORKING
-// NOT REQUIRED
-// Display form to create item
-router.get('/new', function(req, res) {
-  res.send('new route');
-});
-
-/* ----------------- SHOW ROUTE ----------------- */
-
-// NOT REQUIRED
-// Display item with id itemid
-router.get('/:itemid', function(req, res) {
-  console.log(req.params);
-  res.send('Show route');
-});
-
-/* ----------------- CREATE ROUTE ----------------- */
-
-// Create item and add to db
-router.post('/', function(req, res) {
-  res.send('Create route');
-});
-
-/* ----------------- EDIT ROUTE ----------------- */
-
-// NOT WORKING
-// NOT REQUIRED
-// Display form to edit item
-router.get(':itemid/edit', function(req, res) {
-  console.log(req.params);
-  res.send('edit route');
-});
-
-/* ----------------- UPDATE ROUTE ----------------- */
-
-// NOT REQUIRED
-// Update item
-router.put('/:itemid', function(req, res) {
-  console.log(req.params);
-  res.send('update route');
-});
-
-/* ----------------- DESTROY ROUTE ----------------- */
-
-// DELETE ITEM
-router.delete('/:itemid', function(req, res) {
-  console.log(req.params);
-  res.send('delete route');
-});
 
 /* ----------------- EXPORT  ----------------- */
 
