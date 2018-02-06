7e9e03cff12450619214b53c6dfe7e2cbfa59a2a
unknown <aseelaldallal@gmail.com>
2018-02-06 14:02:13 -0500

    change email regular expression to accept uppercase

--- client/src/shared/utility.js
+++ client/src/shared/utility.js
@@ -5,24 +5,23 @@ export const updateObject = (oldObject, updatedProperties) => {
   };
 };
 
-
 export const checkValidity = (value, rules) => {
   let isValid = true;
   if (!rules) {
     return true;
   }
   if (rules.required) {
-    isValid = (value.trim() !== '') && isValid;
+    isValid = value.trim() !== '' && isValid;
   }
   if (rules.minLength) {
-    isValid = (value.replace(/ /g, '').length >= rules.minLength) && isValid;
+    isValid = value.replace(/ /g, '').length >= rules.minLength && isValid;
   }
   if (rules.maxLength) {
-    isValid = (value.replace(/ /g, '').length <= rules.maxLength) && isValid;
+    isValid = value.replace(/ /g, '').length <= rules.maxLength && isValid;
   }
   if (rules.isEmail) {
-    const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-    isValid = pattern.test(value) && isValid
+    const pattern = /[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?/;
+    isValid = pattern.test(value) && isValid;
   }
   return isValid;
-}
+};
