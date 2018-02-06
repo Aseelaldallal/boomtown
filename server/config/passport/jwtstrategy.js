0bc46915b75e356cf4d13245d19e2c11dd18c658
unknown <aseelaldallal@gmail.com>
2018-02-06 13:12:54 -0500

    create login route

--- server/config/passport/jwtstrategy.js
+++ server/config/passport/jwtstrategy.js
@@ -1,6 +1,5 @@
-
 const User = require('../../models/user');
-const passportJWT = require("passport-jwt");
+const passportJWT = require('passport-jwt');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
@@ -8,20 +7,22 @@ let jwtOptions = {};
 jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 jwtOptions.secretOrKey = 'blooper';
 
-
-// for login?
+// to access restricted routes
 const strategy = new JwtStrategy(jwtOptions, (jwt_payload, next) => {
-    console.log('payload recieved: ', jwt_payload);
-    if (jwt_payload.email) { email = email.toLowerCase(); } // Use lower-case e-mails to avoid case-sensitive e-mail matching
-    User.findOne({ 'jwt.email': email }, function (err, user) {
-        if (err) { next(err); }
-        if (user) {
-            next(null, user)
-        } else {
-            next(null, false);
-        }
-    })
+  console.log('payload recieved: ', jwt_payload);
+  if (jwt_payload.email) {
+    email = email.toLowerCase();
+  } // Use lower-case e-mails to avoid case-sensitive e-mail matching
+  User.findOne({ 'jwt.email': email }, function(err, user) {
+    if (err) {
+      next(err);
+    }
+    if (user) {
+      next(null, user);
+    } else {
+      next(null, false);
+    }
+  });
 });
-
 
 module.exports = { strategy };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,48 +7,82 @@ const passport = require('../config/passport'); // index.js
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const router = express.Router();
-const middleware = require("../middleware");
+const middleware = require('../middleware');
+
+const jwt_secretOrKey = 'blooper';
 
 // ===============================================
 // Register
 // ===============================================
 
-router.post('/register', middleware.sanitizeUserInput, middleware.validateRegisterationForm, (req, res) => {
-  User.findOne({ 'jwt.email': req.body.email }, (err, foundUser) => {
-    if (err) {
-      console.log("Fail Case 1");
-      res.status(400).json({ messages: [err.message] });
-    } else if (foundUser) {
-      console.log("Fail Case 2");
-      res.status(400).json({ messages: [`${req.body.email} account already exists`] });
-    } else {
-      const newUser = new User({});
-      newUser.jwt.email = req.body.email;
-      newUser.jwt.password = newUser.generateHash(req.body.password);
-      newUser.fullname = req.body.fullname;
-      newUser.bio = req.body.bio;
-      newUser.save().then((savedUser) => {
-        let payload = { id: savedUser._id };
-        let token = jwt.sign(payload, 'blooper');
-        let expiry = 3600;
-        res.status(200).json({ id: savedUser._id, token: token, expiry: expiry });
-      }).catch(err => {
-        console.log("Fail Case 3");
-        res.status(400).send({ messages: [err.message] })
-      });
-    }
-  })
-})
+router.post(
+  '/register',
+  middleware.sanitizeUserInput,
+  middleware.validateRegisterationForm,
+  (req, res) => {
+    User.findOne({ 'jwt.email': req.body.email }, (err, foundUser) => {
+      if (err) {
+        console.log('Fail Case 1');
+        res.status(400).json({ messages: [err.message] });
+      } else if (foundUser) {
+        console.log('Fail Case 2');
+        res
+          .status(400)
+          .json({ messages: [`${req.body.email} account already exists`] });
+      } else {
+        const newUser = new User({});
+        newUser.jwt.email = req.body.email;
+        newUser.jwt.password = newUser.generateHash(req.body.password);
+        newUser.fullname = req.body.fullname;
+        newUser.bio = req.body.bio;
+        newUser
+          .save()
+          .then(savedUser => {
+            let payload = { id: savedUser._id };
+            let token = jwt.sign(payload, jwt_secretOrKey, { expiresIn: '1h' });
+            let expiry = 3600; // 1hr
+            res
+              .status(200)
+              .json({ id: savedUser._id, token: token, expiry: expiry });
+          })
+          .catch(err => {
+            console.log('Fail Case 3');
+            res.status(400).json({ messages: [err.message] });
+          });
+      }
+    });
+  }
+);
 
 // ===============================================
 // Login
 // ===============================================
 
-// router.post('/login', passport.authenticate('local', (err, req, res) => {
-//     console.log('[Login Post Route]');
-//     console.log(req.user);
-//     console.log('Req: ', req);
-// }));
+router.post('/login', (req, res) => {
+  // Backend Validation
+  User.findOne({ 'jwt.email': req.body.email })
+    .then(foundUser => {
+      if (!foundUser) {
+        res
+          .status(401)
+          .json({ messages: [`${req.body.email} is not registered`] });
+      } else if (foundUser.validPassword(req.body.password)) {
+        let token = jwt.sign({ id: foundUser._id }, jwt_secretOrKey, {
+          expiresIn: '1h'
+        });
+        let expiry = 3600; // 1hr
+        res
+          .status(200)
+          .json({ id: foundUser._id, token: token, expiry: expiry });
+      } else {
+        res.status(401).json({ messages: ['Incorrect Password'] });
+      }
+    })
+    .catch(err => {
+      console.log('Error', err);
+      res.status(401).json({ messages: ['Woops, something went wrong'] });
+    });
+});
 
 // ===============================================
 // Logout
