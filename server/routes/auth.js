c80f7fd71a7aecd1bc637d3f924a75b700047081
unknown <aseelaldallal@gmail.com>
2018-02-06 13:58:55 -0500

    bug fix: ignore case when looking for user email in db, login route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -20,34 +20,39 @@ router.post(
   middleware.sanitizeUserInput,
   middleware.validateRegisterationForm,
   (req, res) => {
-    User.findOne({ 'jwt.email': req.body.email }, (err, foundUser) => {
-      if (err) {
-        res.status(400).json({ messages: [err.message] });
-      } else if (foundUser) {
-        res
-          .status(400)
-          .json({ messages: [`${req.body.email} account already exists`] });
-      } else {
-        const newUser = new User({});
-        newUser.jwt.email = req.body.email;
-        newUser.jwt.password = newUser.generateHash(req.body.password);
-        newUser.fullname = req.body.fullname;
-        newUser.bio = req.body.bio;
-        newUser
-          .save()
-          .then(savedUser => {
-            let payload = { id: savedUser._id };
-            let token = jwt.sign(payload, jwt_secretOrKey, { expiresIn: '1h' });
-            let expiry = 3600; // 1hr
-            res
-              .status(200)
-              .json({ id: savedUser._id, token: token, expiry: expiry });
-          })
-          .catch(err => {
-            res.status(400).json({ messages: [err.message] });
-          });
+    User.findOne(
+      { 'jwt.email': req.body.email.toLowerCase() },
+      (err, foundUser) => {
+        if (err) {
+          res.status(400).json({ messages: [err.message] });
+        } else if (foundUser) {
+          res
+            .status(400)
+            .json({ messages: [`${req.body.email} account already exists`] });
+        } else {
+          const newUser = new User({});
+          newUser.jwt.email = req.body.email;
+          newUser.jwt.password = newUser.generateHash(req.body.password);
+          newUser.fullname = req.body.fullname;
+          newUser.bio = req.body.bio;
+          newUser
+            .save()
+            .then(savedUser => {
+              let payload = { id: savedUser._id };
+              let token = jwt.sign(payload, jwt_secretOrKey, {
+                expiresIn: '1h'
+              });
+              let expiry = 3600; // 1hr
+              res
+                .status(200)
+                .json({ id: savedUser._id, token: token, expiry: expiry });
+            })
+            .catch(err => {
+              res.status(400).json({ messages: [err.message] });
+            });
+        }
       }
-    });
+    );
   }
 );
 
