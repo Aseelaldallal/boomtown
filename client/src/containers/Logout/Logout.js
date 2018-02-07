d33c616161e96d580848c2b29f60932fe572d3b8
unknown <aseelaldallal@gmail.com>
2018-02-06 21:33:39 -0500

    create logout component

---
+++ client/src/containers/Logout/Logout.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../store/actions/';
+import { Redirect } from 'react-router-dom';
+
+class Logout extends Component {
+  componentDidMount() {
+    this.props.logout();
+  }
+
+  render() {
+    return <Redirect to="/" />;
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    logout: () => dispatch(actions.logout())
+  };
+};
+
+export default connect(null, mapDispatchToProps)(Logout);
