ecf52d5293a5e394b24740aca20849664904b9bf
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-02-12 11:37:46 -0500

    temp , might delete, not used now

---
+++ client/src/containers/Share/ShareContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Share from './Share';
+
+class ShareContainer extends Component {
+  render() {
+    return (
+      <div>
+        <Share />
+      </div>
+    );
+  }
+}
+
+export default ShareContainer;
