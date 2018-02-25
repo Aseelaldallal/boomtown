39e7a3c22e23cdf3035287575b5d496d17bff2b9
aseelaldallal <aseelaldallal@gmail.com>
2018-02-25 16:31:35 -0500

    switch db to mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const seedDB = require('./seed');
 
 mongoose.Promise = global.Promise; // Get promises from Node
-
+console.log("DB", process.env.BOOMTOWN_DB_URI);
 mongoose.connect(
   process.env.BOOMTOWN_DB_URI || 'mongodb://localhost:27017/boomtown'
 );
